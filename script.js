1dfc1d08625bdf47e4ec8454460281a6b37f78d5
depauque <xplumbeerx@gmail.com>
2025-03-27 00:38:38 +0300

    Local storage, checkboxes, minor fixes

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ const itemsList = document.querySelector(".container");
 const addBtn = document.getElementById("add");
 const removeBtn = document.getElementById("remove");
 
+window.onload = function () {
+  item.focus();
+  loadItems();
+};
+
 addBtn.addEventListener("click", addItem);
 removeBtn.addEventListener("click", removeItem);
+
+//event listeners
 
 item.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
@@ -13,13 +20,27 @@ item.addEventListener("keydown", function (event) {
   }
 });
 
+itemsList.addEventListener("change", (event) => {
+  if (event.target.classList.contains("toggle-checkbox")) {
+    const paragraph = event.target.parentElement;
+    paragraph.classList.toggle("completed-item");
+    saveItems();
+  }
+});
+
+//functions
+
 function addItem() {
   const value = item.value.trim();
   if (value) {
+    const itemCount = itemsList.querySelectorAll("p").length + 1;
     itemsList.insertAdjacentHTML(
       "beforeend",
-      `<p>${itemsList.querySelectorAll("p").length + 1}. ${value}</p>`
+      `<p>
+      <input type="checkbox" class="toggle-checkbox">
+      ${itemCount}. ${value}</p>`
     );
+    saveItems();
     item.value = "";
     item.focus();
   }
@@ -27,8 +48,28 @@ function addItem() {
 
 function removeItem() {
   itemsList.lastElementChild?.remove();
+  saveItems();
 }
 
-window.onload = function () {
-  item.focus();
-};
+function saveItems() {
+  const items = Array.from(itemsList.querySelectorAll("p")).map((p) => ({
+    text: p.textContent,
+    completed: p.classList.contains("completed-item"),
+  }));
+  localStorage.setItem("todoItems", JSON.stringify(items));
+}
+
+function loadItems() {
+  let arrItems = JSON.parse(localStorage.getItem("todoItems")) || [];
+  arrItems.forEach((item) => {
+    const newItem = `
+    <p class="${item.completed ? "completed-item" : ""}">
+    <input type = "checkbox" class="toggle-checkbox" ${
+      item.completed ? "checked" : ""
+    }>
+  ${item.text}
+    </p>
+    `;
+    itemsList.insertAdjacentHTML("beforeend", newItem);
+  });
+}
